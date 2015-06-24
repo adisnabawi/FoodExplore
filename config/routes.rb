560c12b17b7ebc2791b9433b6c250138a65b1d90
adisnabawi <adisazizan@gmail.com>
2015-06-24 20:41:06 +0000

    Adding the header and footer with login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -8,6 +10,10 @@ Rails.application.routes.draw do
   get 'static_pages/recipe'
 
   get 'static_pages/about'
+  
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  # delete 'logout'  => 'sessions#destroy
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
