75ca143f6e2c046cf1d1fef4bf8f4947bb56fa86
adisnabawi <adisazizan@gmail.com>
2015-06-25 06:21:46 +0000

    Help pages updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,26 @@ class UsersController < ApplicationController
   @user = User.new(user_params)
   if @user.save
   log_in @user
-  flash[:success] = "Welcome to the Sample App!"
+  flash[:success] = "Finish updating!"
   redirect_to @user
   else
   render 'new'
   end
   end
   
+  def edit
+  @user = User.find(params[:id])
+  end
+  
+  def update
+  @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+  # Handle a successful update.
+  else
+  render 'edit'
+  end
+  end
+
   private
   def user_params
   params.require(:user).permit(:name, :email, :password,
